d2822dc7eaee4da56345eb8efb615c6ddbbf46e4
fre1302 <freddyw1302@gmail.com>
2019-01-20 13:55:55 +0100

    Completed the pwm library

--- src/driver_node/src/main.cc
+++ src/driver_node/src/main.cc
@@ -3,7 +3,7 @@
 
 #include "ros/ros.h"
 #include "driver_node/driver_srv.h"
-#include "pwm.h"
+#include "pwm.hh"
 
 bool serviceCallback(driver_node::driver_srv::Request  &req,
          driver_node::driver_srv::Response &res)
@@ -17,7 +17,7 @@ bool serviceCallback(driver_node::driver_srv::Request  &req,
 int main(int argc, char **argv){
   int input;
   ros::init(argc,argv,"driver_node");
-  pwm_set_enable(3);
+  pwm_set_enable("PWM0");
   ros::NodeHandle n;
   ros::ServiceServer service = n.advertiseService("master_driver", serviceCallback);
   ros::spin();
