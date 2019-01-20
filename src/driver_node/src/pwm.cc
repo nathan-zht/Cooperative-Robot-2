a4aadbea8e0cf8ee8738c3dcd649cd25703a6552
frew1302 <freddyw1302@gmail.com>
2019-01-20 14:38:22 +0000

    Created function to handle service request

--- src/driver_node/src/pwm.cc
+++ src/driver_node/src/pwm.cc
@@ -2,7 +2,7 @@
 #include <string>
 #include <sys/stat.h>
 #include "pwm.hh"
-#define BASE_PATH "sys/class/PWM/"
+#define BASE_PATH "/sys/class/PWM/"
 std::ofstream sysfs;
 static bool fileExists(const std::string& filename)
 {
@@ -15,9 +15,9 @@ static bool fileExists(const std::string& filename)
 }
 
 SYSFS_RES pwm_set_duty(std::string reg, int dutycycle){
-  std::string path = BASE_PATH + reg + "DUTY";
+  std::string path = BASE_PATH + reg + "/DUTY";
   if (fileExists(path)){
-	  sysfs.open(path);
+	  sysfs.open(path.c_str());
 	  sysfs << dutycycle;
 	  sysfs.close();
 	  return SUCCESS;
@@ -25,9 +25,9 @@ SYSFS_RES pwm_set_duty(std::string reg, int dutycycle){
 	  return FAILED;
 }
 SYSFS_RES pwm_set_period(std::string reg, int period){
-	std::string path = BASE_PATH + reg + "PERIOD";
+	std::string path = BASE_PATH + reg + "/PERIOD";
 	if (fileExists(path)){
-		sysfs.open(path);
+		sysfs.open(path.c_str());
 		sysfs << period;
 		sysfs.close();
 		return SUCCESS;
@@ -36,9 +36,9 @@ SYSFS_RES pwm_set_period(std::string reg, int period){
 }
 
 SYSFS_RES pwm_set_enable(std::string reg){
-	std::string path = BASE_PATH + reg + "ENABLE";
+	std::string path = BASE_PATH + reg + "/ENABLE";
 	if (fileExists(path)){
-		sysfs.open(path);
+		sysfs.open(path.c_str());
 		sysfs << 3;
 		sysfs.close();
 		return SUCCESS;
