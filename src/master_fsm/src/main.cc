ce109e53784fd65291ebde51f0e3a2ce142d71b4
frew1302 <freddyw1302@gmail.com>
2019-01-20 18:58:42 +0000

    Changed current state variable. Previously not a pointer and would not work. Added sleep function to loop to ensure executeCommand is executed when not in idle, and to make the rosinfo output more readable. Previously it can take a while for master_fsm to reply to service request. Added Test points to various functions.

--- src/master_fsm/src/main.cc
+++ src/master_fsm/src/main.cc
@@ -11,7 +11,7 @@ typedef enum State{
 
     State curr_state = IDLE;
 */
-
+std::string val;
 class State {
   /*
   class to implement interface.
@@ -55,7 +55,12 @@ class ManualControl : public State{
   }
   
   void executeCommand(){
-    std::cout << std::endl <<"Going to coordinates x: " << x_coordinate << " y: " << y_coordinate << std::endl;
+    ROS_INFO("[Master_FSM] Executing movement to coordinate from manual input...");
+    //testing
+    //std::cout << std::endl << "Manual Control Command Execute Going to coordinates x: " << x_coordinate << " y: " << y_coordinate << std::endl;
+    //if the following are uncommented, the process will  halt until there is input from user.
+    //std::cout << "Enter a new value: " << std::endl;
+    //std::cin >> val;
   }
   
   std::string getState(){
@@ -66,20 +71,25 @@ class ManualControl : public State{
 
 Idle idle;
 ManualControl manual;
-State current_state = idle;
+State *current_state = &idle;
 
 bool fsm(master_fsm::ServerListener::Request &req, 
   master_fsm::ServerListener::Response &res){
   //state machine here
   //return true when service is complete
   //res.status;
-  
-  if (current_state.getState() == "IDLE"){
+  std::string coor_x;
+  std::string coor_y;
+  if ((current_state->getState()) == "IDLE"){
     res.status = "free";
     if(req.command == "manual"){
-      current_state = manual;
-      current_state.setCoordinate(req.coordinate_x, req.coordinate_y);
-      res.status = "busy";
+      current_state = &manual;
+      current_state->setCoordinate(req.coordinate_x, req.coordinate_y);
+      coor_x = req.coordinate_x;
+      coor_y = req.coordinate_y;
+      //current_state->executeCommand();
+      ROS_INFO("[Master_FSM] Coordinate x: %d, Coordinate y: %d", req.coordinate_x, req.coordinate_y);
+      res.status = "busy" + coor_x + coor_y; //+ req.coordinate_x + req.coordinate_y;
     }
   } 
 };
@@ -88,16 +98,23 @@ int main(int argc, char **argv){
   //initialization
   ros::init(argc, argv, "master_fsm");
   ros::NodeHandle n;
+  
+  ros::Rate loop_rate(10); //in hz
+  //ros::Duration().sleep may also be an option
+  
   //service server node init
   ros::ServiceServer service = n.advertiseService("server_listener", fsm);
   ros::AsyncSpinner spinner(1); // Use 1 thread
   spinner.start();
   ROS_INFO("[Master_FSM] Master FSM ready");
   while(ros::ok()){
-    current_state.executeCommand();
-    current_state = idle;
+    current_state->executeCommand();
+    /*current_state->executeCommand();
+    may need to be executed more than once or
+    it may not be execute outside of idle*/
+    current_state = &idle;
+    loop_rate.sleep();
   }
-  
 /*
   service client node init
   ros::ServiceClient client = n.serviceClient<master_fsm::ServerListener>("server_listener");
