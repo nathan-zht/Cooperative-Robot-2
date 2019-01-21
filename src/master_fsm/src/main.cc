d4ebb2c1297c6ae4fe73225eab03b4d495e1dfe4
zybo2 <zybo2@xillinux.com>
2019-01-21 00:00:20 +0000

    added publisher and subscribers for master and slave statuses, and right and left encoders. added dummy handlers. Tidied up code.

--- src/master_fsm/src/main.cc
+++ src/master_fsm/src/main.cc
@@ -2,31 +2,34 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "master_fsm/ServerListener.h"
-/*
-using namespace std;
-typedef enum State{
-  IDLE,
-  MANUALCONTROL
-}State;
+#include "master_fsm/status.h"
+#include "encoder_node/encoder_msg.h"
 
-    State curr_state = IDLE;
-*/
-std::string val;
+/* variable initialization */
+ros::Publisher master_status_pub; 
+ros::Subscriber slave_status_sub;
+ros::Subscriber left_enc_sub;
+ros::Subscriber right_enc_sub;
+master_fsm::status master_msg;
+bool callbacktriggered = false;
+
 class State {
-  /*
-  class to implement interface.
-  remember only virtual methods should be defined here.
-  These can be overriden in individual class definitions.
-  */
+  /* class to implement interface.
+  only virtual methods should be defined here.
+  These can be overriden in individual class definitions. */
+  
   public:
   State(){};
   virtual ~State(){}; 
   virtual void executeCommand(){};
   virtual std::string getState(){};
   virtual void setCoordinate(int x, int y){};
+  
   protected:
   std::string state_id;
 };
+
+/* State Definition */
 
 class Idle : public State {
   public:
@@ -63,7 +66,6 @@ class ManualControl : public State{
   std::string getState(){
     return "MANUAL";
   }
-  void setState(){}
 };
 
 class CrossFormation : public State {
@@ -94,19 +96,35 @@ class OrbitFormation : public State {
   }
 };
 
+class Rotate : public State {
+  public:
+  Rotate(){
+    State::state_id = "ROTATE";
+  }
+  ~Rotate(){};
+  void executeCommand(){
+    ROS_INFO("[Master_FSM] Executing Rotate...");
+  }
+  std::string getState(){ 
+    return "ROTATE";
+  }
+};
+
+/* declare state class instances */
 Idle idle;
 ManualControl manual;
 CrossFormation cross;
 OrbitFormation orbit;
+Rotate rotate;
 State *current_state = &idle;
 
-bool callbacktriggered = false;
+/* callbacks */
 
-bool fsm(master_fsm::ServerListener::Request &req, 
-  master_fsm::ServerListener::Response &res){
+bool fsm(master_fsm::ServerListener::Request &req, master_fsm::ServerListener::Response &res){  
   //state machine here
   std::string coor_x;
   std::string coor_y;
+  
   if ((current_state->getState()) == "IDLE"){
     //res.status = "free";
     if(req.command == "manual"){
@@ -118,36 +136,72 @@ bool fsm(master_fsm::ServerListener::Request &req,
       //current_state->executeCommand();
       ROS_INFO("[Master_FSM] Coordinate x: %d, Coordinate y: %d", req.coordinate_x, req.coordinate_y);
       res.status = "SUCCESS"; //+ req.coordinate_x + req.coordinate_y;
+      master_msg.state = 1; //busy
+    
     }else if(req.command == "cross"){
       ROS_INFO("[Master_FSM] Cross Desination Coordinates: ");      
       res.status = "SUCCESS";
+      master_msg.state = 1;
+    
     }else if(req.command == "orbit"){
       ROS_INFO("[Master_FSM] Orbit Desination Coordinates: ");      
       res.status = "SUCCESS";
-    }else {}
+      master_msg.state = 1;
+    
+    }else if(req.command == "rotate"){
+      ROS_INFO("[Master_FSM] Rotate: ");      
+      res.status = "SUCCESS";
+      master_msg.state = 1;
+    }
+    
   }else{
     res.status = "NOTSUCCESS";
+    master_msg.state = 0;
   }
+  
   return true;
 };
 
+void slaveStatusCallback(const master_fsm::status& slave_status){
+  ROS_INFO("[master_fsm] Slave state: %d", slave_status.state);
+}
+
+void rightEncCallback(const encoder_node::encoder_msg right_enc){
+  //ROS_INFO("[master_fsm] right encoder value: %d", right_enc->data.c_str());
+  ROS_INFO("[master_fsm] Right encoder data | ticks: %d direction", right_enc.ticks, right_enc.direction);
+}
+
+void leftEncCallback(const encoder_node::encoder_msg left_enc){
+  ROS_INFO("[master_fsm] Left encoder data | ticks: %d direction", left_enc.ticks, left_enc.direction);
+}
+
 int main(int argc, char **argv){
-  //initialization
+  //ros initialization
   ros::init(argc, argv, "master_fsm");
   ros::NodeHandle n;
   
-  ros::Rate loop_rate(10); //in hz
+  //init sleep
   //ros::Duration().sleep may also be an option
+  ros::Rate loop_rate(10); //in hz
+
+  //init publishers and subscribers
+  master_status_pub = n.advertise<master_fsm::status>("master_status", 1000); 
+  slave_status_sub = n.subscribe("slave_status", 1000, slaveStatusCallback);
+  right_enc_sub = n.subscribe("left_enc", 1000, rightEncCallback);
+  left_enc_sub = n.subscribe("right_enc", 1000, leftEncCallback);
   
-  //service server node init
+  //service server node initialization
   ros::ServiceServer service = n.advertiseService("server_listener", fsm);
   ros::AsyncSpinner spinner(1); // Use 1 thread
   spinner.start();
+  
   ROS_INFO("[Master_FSM] Master FSM ready");
+  
   while(ros::ok()){
     current_state->executeCommand();
+    master_status_pub.publish(master_msg);
     /*current_state->executeCommand();
-    may need to be executed more than once or
+        may need to be executed more than once or
     it may not be execute outside of idle*/
    /*alternative to sleep is the callbacktriggered flag but rosinfo is unreadable
    if (callbacktriggered == true){
